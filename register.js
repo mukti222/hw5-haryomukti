ecdac049719138c9b48ec4512e82b37143ada87b
mukti222 <135799527+mukti222@users.noreply.github.com>
2023-09-09 12:52:12 +0700

    Update register.js

--- register.js
+++ register.js
@@ -74,15 +74,17 @@ class DataHandler {
       }
 
       if (storedDataUmur && storedDataUmur.length > 0) {
-        const ratasangu = calculateAverage(storedDataSangu);
-        const rataumur = calculateAverage(storedDataUmur);
-        const buatumur = document.createElement("td");
-        const umur1 = document.createTextNode(
-          'Rata" Umur : ' +
-            rataumur.toFixed(2) +
-            ", dan Rata\" sangu  : Rp." +
-            ratasangu.toFixed(2)
-        );
+    const ratasangu = calculateAverage(storedDataSangu);
+    const rataumur = calculateAverage(storedDataUmur);
+    const buatumur = document.createElement("td");
+    let formattedRataUmur;
+
+
+    if (rataumur === Math.floor(rataumur)) {
+        formattedRataUmur = 'Rata" Umur : ' + rataumur + ", dan Rata\" sangu  : Rp." + ratasangu.toFixed(2);
+    } else {
+        formattedRataUmur = 'Rata" Umur : ' + rataumur.toFixed(2) + ", dan Rata\" sangu  : Rp." + ratasangu.toFixed(2);
+    } const umur1 = document.createTextNode(formattedRataUmur);
         buatumur.appendChild(umur1);
         document.getElementById("buatresume").appendChild(buatumur);
         buatumur.colSpan = storedDataSangu.length;
